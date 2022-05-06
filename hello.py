4a1b5daf974cb17a9162140dc33f37db47b2b88d
{JaiyoungJoo} <{wodudwn@daum.net}>
2022-05-06 13:23:02 +0900

    this is work
    he gives me 3 questions

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
 print('hello')
+#과제 1부터 20까지 3의 배수는 year 출력하시고 나머지는 숫자 그대로
+#5의 배수 dream
+#15dmlqotn yeardream
+
+for i in range(1, 21):
+    if i % 15 == 0:
+        print('yeardream')
+    elif i % 5 == 0 :
+        print('dream')
+    elif i % 3 == 0 :
+        print('year')
+    else :
+        print(i)
+
+# vi hello.py
+# git status
+# git add hello.py
+# git commit
